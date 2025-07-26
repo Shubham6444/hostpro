6af7957e9bf39380daf2b960e1da5c71569a2221
unknown <skm11794@gmail.com>
2025-07-26 12:17:42 +0530

    Added

--- microservices/app2/server.js
+++ microservices/app2/server.js
@@ -22,7 +22,7 @@ const userRoutes = require("./routers/adminRoutes/vm_mappings");
 const sshRouter = require("./routers/ssh2connt");
 
 // Middleware
-const allowedOrigins = ['http://localhost:8080', 'http://localhost:3000'];
+const allowedOrigins = ['https://remixorbit.in/', 'http://localhost:3000'];
 app.use("/", sshRouter); // ✅ WebSocket SSH router
 
 app.use(cors({
